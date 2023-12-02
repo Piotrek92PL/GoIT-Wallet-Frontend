14d596af162da18919fb07e3a015d7b1d621a8cd
Asia <dobrowolska.krakow@gmail.com>
2023-12-02 13:20:26 +0100

    added logic to close moda by click on backdrop and escape

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import css from './ModalLogout.module.css';
 import { ButtonConfirmLogout } from 'components/ButtonConfirmLogout/ButtonConfirmLogout';
 import { ButtonCancelLogout } from 'components/ButtonCancelLogout/ButtonCancelLogout';
@@ -7,8 +7,29 @@ export const ModalLogout = ({ isOpen, toggleModal, logout }) => {
   const backdropClasses = `${css.backdrop} ${
     !isOpen ? css['backdrop-is-hidden'] : ''
   }`;
+
+  const handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      toggleModal();
+    }
+  };
+
+  useEffect(() => {
+    const handleEscapeKey = event => {
+      if (event.key === 'Escape') {
+        toggleModal();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscapeKey);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, [toggleModal]);
+
   return (
-    <div className={backdropClasses}>
+    <div className={backdropClasses} onClick={handleBackdropClick}>
       <div className={css.modal}>
         <p className={css.modalQuery}>Are you sure you want to logout?</p>
         <div className={css.buttonContainer}>
