482b29ad282333122c95ce3bebdf80779961a090
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-12-06 17:54:25 +0100

    added basic styles to ModalAddTransaction

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from './ModalAddTransaction.module.css';
 
 export const ModalAddTransaction = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div>
-      <p>Add Transaction</p>
-      <button onClick={onClose}>Close</button>
+    // <div>
+    //   <p>Add Transaction</p>
+    //   <button onClick={onClose}>Close</button>
+    // </div>
+    <div className={css.modalOverlay}>
+      <div className={css.modal}>
+        <div className={css.modalContent}>
+          <button className={css.closeButton} onClick={onClose}>
+            x
+          </button>
+          <p>Add Transaction</p>
+        </div>
+      </div>
     </div>
   );
 };
