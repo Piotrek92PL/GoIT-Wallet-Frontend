759fca2a3c6779601fca6b93a7b0d3ec06da7ed3
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-12-06 17:53:38 +0100

    added Balance component and styles

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,1 +1,11 @@
-export function Balance() { }
+import React from 'react';
+import css from './Balance.module.css';
+
+export function Balance() {
+  return (
+    <div className={css.balanceContainer}>
+      <p className={css.balance}>Your balance</p>
+      <p className={css.deposit}>₴ 24 000.00</p>
+    </div>
+  );
+}
