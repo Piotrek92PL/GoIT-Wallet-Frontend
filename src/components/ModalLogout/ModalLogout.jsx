3355cb2802b8ac95b060dd528834a8743a1fa6db
Asia <dobrowolska.krakow@gmail.com>
2023-12-06 14:16:48 +0100

    addad styles to hold components on Pages

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import css from './ModalLogout.module.css';
 import { ButtonConfirmLogout } from 'components/ButtonConfirmLogout/ButtonConfirmLogout';
 import { ButtonCancelLogout } from 'components/ButtonCancelLogout/ButtonCancelLogout';
 import PropTypes from 'prop-types';
 
 export const ModalLogout = ({ isOpen, toggleModal, logout }) => {
-  const backdropClasses = `${css.backdrop} ${
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTabletDesktop = useMediaQuery({ minWidth: 769 });
+
+  let backdropStyle = css.backdrop;
+  let modalStyle = css.modal;
+
+  if (isTabletDesktop) {
+    backdropStyle += ` ${css.backdropTabletDesktop}`;
+  } else if (isMobile) {
+    backdropStyle += ` ${css.backdropMobile}`;
+    modalStyle += ` ${css.modalMobile}`;
+  }
+
+  const backdropClasses = `${backdropStyle} ${
     !isOpen ? css['backdrop-is-hidden'] : ''
   }`;
 
@@ -31,7 +45,7 @@ export const ModalLogout = ({ isOpen, toggleModal, logout }) => {
 
   return (
     <div className={backdropClasses} onClick={handleBackdropClick}>
-      <div className={css.modal}>
+      <div className={modalStyle}>
         <p className={css.modalQuery}>Are you sure you want to logout?</p>
         <div className={css.buttonContainer}>
           <ButtonCancelLogout onClick={toggleModal} />
